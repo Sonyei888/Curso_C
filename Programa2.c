69c7e24e00163f6e5ba21c5c1d1c5d4cc4ae8e90
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2022-11-10 14:56:38 -0500

    Programa 3

--- Programa2.c
+++ Programa2.c
@@ -5,7 +5,7 @@ int main()
     int superficie;
     printf("Ingrese el valor del lado del cuadrado: ");
     scanf("%i", &lado);
-    superficie = lado+lado;
+    superficie = lado+lad;
     printf("La superficie del cuadrado es: ");
     printf("%i", superficie);
 }
