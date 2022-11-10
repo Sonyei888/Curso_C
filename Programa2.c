26ea730a054f5ec04ae3411d61ffb57f7fba5b1e
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2022-11-10 14:42:45 -0500

    Proyecto 2

---
+++ Programa2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int lado;
+    int superficie;
+    printf("Ingrese el valor del lado del cuadrado: ");
+    scanf("%i", &lado);
+    superficie = lado+lado;
+    printf("La superficie del cuadrado es: ");
+    printf("%i", superficie);
+}
