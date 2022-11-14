40f357b2b6c2f138868f4944a31818e25563aafb
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2022-11-14 15:12:10 -0500

    Programa 8

---
+++ Programa8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    float sueldo;
+    printf("Ingrese el sueldo: ");
+    scanf("%f", &sueldo);
+    if(sueldo>3000){
+        printf("Esta persona debe abonar impuesto");
+    }
+    getch();
+    return 0;
+}
